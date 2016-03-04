71f9c88d027c40369892c8c9e49167de01fadf51
ronliu <ronliu2k@gmail.com>
2016-03-04 15:36:20 +1100

    minor updates.

--- common/layout/header.js
+++ common/layout/header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Menu, Icon} from 'antd';
 import {Link} from 'react-router';
 
-export default class Header extends React.Component {
+export default class extends React.Component {
 	render() {
 		return <Menu mode="horizontal">
-			<Menu.Item key="mail">
-				<Icon type="mail" />导航一
+			<Menu.Item key="home">
+				<Icon type="home" />主页
 			</Menu.Item>
  			<Menu.Item key="auth">
-			    <Link to="/auth">登陆</Link>
+			     <Link to="/auth"><Icon type="user" />登陆</Link>
 			</Menu.Item>
 		</Menu>
 	}

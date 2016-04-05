318ba98a7111588b14600ed2179ddf27a08fbfc6
ronliu <ronliu2k@gmail.com>
2016-04-06 09:50:51 +1000

    removed immutable

--- common/shared/utils/tools.spec.js
+++ common/shared/utils/tools.spec.js
@@ -1,5 +1,4 @@
 import {expect} from 'chai';
-import {is, fromJS} from 'immutable';
 
 export const testReducer = (reducer, cases) => {
 	cases.forEach((x, index)=>{
@@ -14,16 +13,5 @@ export const testReducer = (reducer, cases) => {
 ${JSON.stringify(x, null, '  ')}		
 actual: ${JSON.stringify(actual, null, '  ')}`);
 		}
-	});
-};
-
-export const testReducerUsingImmutableJs = (reducer, cases) => {
-	cases.forEach((x, index)=>{
-		let {before, action, after} = x;
-		let actual = reducer(fromJS(before), action);
-		let passed = is(actual, fromJS(after));
-		if (!passed) throw new Error(`#${index + 1}: failed, 
-${JSON.stringify(x, null, '  ')}		
-actual: ${JSON.stringify(actual, null, '  ')}`);
 	});
 };
